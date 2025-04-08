25d8dc4905367946b49a18aecf10721a6a89386e
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-08 16:51:39 +0000

    Added 3-02 template for Celsius

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Sasha. Vorontsov
+// Created on: April 2025
 // This file contains the JS functions for index.html
+
+function calculatehoursworkedAndhourlywage() {
+  // input
+  const TaxAmount= 3 
+  const FUnits = parseFloat(document.getElementById("fahrenheit").value);
+
+  let Celsius = (FUnits - 32)* (5/9)
+  let comment = ("Thanks for using our service")
+  
+  document.getElementById('answer1').innerHTML = 
+    `Celsius is ${Celsius.toFixed(2)} units <br>
+    Comment: ${comment.toFixed(2)}`
+
+}
