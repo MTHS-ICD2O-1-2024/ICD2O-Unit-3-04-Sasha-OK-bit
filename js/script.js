31a7086a5139a3b0e854c67959e5e5c805b97286
Sasha-OK-bit <sasha.vorontsov@stu.ocsb.ca>
2025-04-08 17:19:22 +0000

    Wow

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@
 
 function calculatefahernheittocelsius() {
   // input
-  const TaxAmount= 3 
-  const FUnits = parseFloat(document.getElementById("fahrenheit").value);
 
-  let Celsius = (FUnits - 32)* (5/9)
+  const funits = parseFloat(document.getElementById("fahrenheit").value);
+
+  let Celsius = (funits - 32)* (5/9)
   let comment = ("Thanks for using our service")
   
   document.getElementById('answer1').innerHTML = 
